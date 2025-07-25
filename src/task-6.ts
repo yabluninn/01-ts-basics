605fe9feec1b38af66aa49364143969e9bfd53b8
yabluninn <yablunin.artem@gmail.com>
2025-07-25 13:07:32 +0300

    fixed some issues

--- src/task-6.ts
+++ src/task-6.ts
@@ -2,10 +2,10 @@ function getFirstElement<T>(arr: Array<T>): T {
   return arr[0];
 }
 
-console.log("Task 6: ");
+console.log("Task 6:");
 
-console.log(getFirstElement([1, 2, 3])); // 1);
-console.log(getFirstElement(["a", "b", "c"])); // "a"
-console.log(getFirstElement([true, false, true])); // true);
+console.log(getFirstElement<number>([1, 2, 3])); // 1
+console.log(getFirstElement<string>(["a", "b", "c"])); // "a"
+console.log(getFirstElement<boolean>([true, false, true])); // true
 
 console.log("");
